c7290a392f1bf0954602e0e20d9acd2688fb368e
YevhDr <evgesha.drozdova@gmail.com>
2018-01-05 00:14:20 +0200

    lamp

--- js/animation-on-scroll.js
+++ js/animation-on-scroll.js
@@ -1,46 +1,82 @@
-$(window).scroll(function () {
-    var elems = document.querySelectorAll(".one"),
-        i;
+// $(window).scroll(function () {
+//     var elems = document.querySelectorAll(".one"),
+//         i;
+//
+//     var top_of_element = $("#headers").offset().top; //top of the need elem on scroll
+//     var bottom_of_element = $("#headers").offset().top + $("#headers").outerHeight();
+//     var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+//     var top_of_screen = $(window).scrollTop();
+//
+//
+//     for (i = 0; i < elems.length; ++i) {
+//         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+//             elems[i].className = elems[i].setAttribute('class', 'one logo');
+//         } else {
+//             elems[i].className = elems[i].setAttribute('class', 'one hidden');
+//         }
+//     }
+//
+// });
 
-    var top_of_element = $("#headers").offset().top; //top of the need elem on scroll
-    var bottom_of_element = $("#headers").offset().top + $("#headers").outerHeight();
-    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
-    var top_of_screen = $(window).scrollTop();
+//
+// $(window).scroll(function () {
+//     var elems = document.querySelectorAll(".mission"),
+//         i;
+//
+//     var top_of_element = $("#mission").offset().top; //top of the need elem on scroll
+//     var bottom_of_element = $("#mission").offset().top + $("#mission").outerHeight();
+//     var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+//     var top_of_screen = $(window).scrollTop();
+//
+//
+//     for (i = 0; i < elems.length; ++i) {
+//         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+//             elems[i].className = elems[i].setAttribute('class', 'mission mission-draw');
+//         } else {
+//             elems[i].className = elems[i].setAttribute('class', 'mission hidden');
+//         }
+//     }
+//
+// });
+//
+// $(window).scroll(function () {
+//     var vectors = document.querySelectorAll(".vector"),
+//         i;
+//
+//     var top_of_element = $("#vector").offset().top; //top of the need elem on scroll
+//     var bottom_of_element = $("#vector").offset().top + $("#vector").outerHeight();
+//     var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+//     var top_of_screen = $(window).scrollTop();
+//
+//
+//     for (i = 0; i < vectors.length; ++i) {
+//         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+//             vectors[i].className = vectors[i].setAttribute('class', 'vector vector-draw');
+//         } else {
+//             vectors[i].className = vectors[i].setAttribute('class', 'vector hidden');
+//         }
+//     }
+//
+// });
+//
+// $(window).scroll(function () {
+//     var topic = document.querySelectorAll(".topic"),
+//         i;
+//
+//     var top_of_element = $("#topic").offset().top; //top of the need elem on scroll
+//     var bottom_of_element = $("#topic").offset().top + $("#topic").outerHeight();
+//     var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+//     var top_of_screen = $(window).scrollTop();
+//
+//
+//     for (i = 0; i < topic.length; ++i) {
+//         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+//             topic[i].className = topic[i].setAttribute('class', 'topic topic-draw');
+//         } else {
+//             topic[i].className = topic[i].setAttribute('class', 'topic hidden');
+//         }
+//     }
+//
+// });
 
 
-    for (i = 0; i < elems.length; ++i) {
-        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-            elems[i].className = elems[i].setAttribute('class', 'one logo');
-        } else {
-            elems[i].className = elems[i].setAttribute('class', 'one hidden');
-        }
-    }
-
-});
-
-var $animation_elements = $('.animation-element');
-var $window = $(window);
-
-function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
-
-    $.each($animation_elements, function () {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
-
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) {
-            $element.addClass('in-view');
-        } else {
-            $element.removeClass('in-view');
-        }
-    });
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
